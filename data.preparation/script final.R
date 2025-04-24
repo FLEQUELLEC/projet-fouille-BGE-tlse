83505bb9f00271f4d5dab20aefe535f0ad04def1
Antonin MENARD <antoninmenard444@gmail.com>
2025-04-25 00:43:17 +0200

    modif r et retiré r file qui sont plus utiles

--- data.preparation/script final.R
+++ data.preparation/script final.R
@@ -1,4 +1,4 @@
-# 📦 Chargement des packages nécessaires
+# Chargement des packages nécessaires
 library(tidyverse)    # Manipulation des données
 library(ggplot2)      # Visualisation
 library(patchwork)    # Organisation des graphiques en grilles
@@ -7,9 +7,9 @@ library(FactoMineR)
 library(factoextra)
 library(cluster)
 
-# 📥 Chargement des données depuis différents chemins possibles
+# Chargement des données depuis différents chemins possibles
 data <- read_csv("~/Documents/projet/data/thyroid_data.csv")
-#data <- read_csv("~/projet/dossier_cancer/thyroid_data.csv")
+data <- read_csv("~/projet/dossier_cancer/thyroid_data.csv")
 
 # Résumé général des données (médianes, min, max, etc.)
 summary(data)
@@ -30,7 +30,7 @@ data_num <- data %>%
 data_long <- data_num %>%
   pivot_longer(cols = -Recurred, names_to = "Variable", values_to = "Valeur")
 
-# Création de boxplots pour comparer les variables numériques selon la récidive
+# Création de boxplots pour comparer la variable numérique (age) selon la récidive
 ggplot(data_long, aes(x = Recurred, y = Valeur, fill = Recurred)) +
   geom_boxplot() +
   facet_wrap(~Variable, scales = "free_y") +
@@ -58,6 +58,7 @@ ggplot(data_cat_long, aes(x = Valeur, fill = Recurred)) +
   scale_y_continuous(labels = scales::percent_format()) +
   theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
+
 # Test du Chi² pour chaque variable qualitative (hors Recurred) vs Recurred
 cat_vars <- data %>%
   select(where(is.character)) %>%
@@ -74,20 +75,82 @@ chi_results <- lapply(cat_vars, function(var) {
   bind_rows() %>%
   arrange(p_value)
 
+
+
+# Affichage de tous les barplots des variables qualitatives
+var_quali <- c("Gender", "Hx_Radiothreapy", "Adenopathy", "Pathology", "Focality", 
+               "Risk", "T", "N", "M", "Stage", "Response", "Recurred")
+
+plots <- list()
+for (var in var_quali) {
+  p <- ggplot(data, aes_string(x = var)) +
+    geom_bar(fill = "#0073C2FF") +
+    ggtitle(paste("Distribution des modalités pour", var)) +
+    xlab(var) + ylab("Nombre d'individus") +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  plots[[var]] <- p
+}
+wrap_plots(plots, ncol = 3)  # Affichage organisé 
+#(utiliser le "zoom" pour voir le résutlat bien affiché)
+
+# Taux global de récidive
+nrow(data[data$Recurred == 'Yes', ]) / nrow(data)  # ~28 %
+
+
+# Comparaison des âges selon la récidive
+
+age_yes <- data$Age[data$Recurred == "Yes"]
+age_no  <- data$Age[data$Recurred == "No"]
+
+# Test de variance
+var.test(age_yes, age_no)
+
+# Test de normalité
+shapiro.test(age_yes)
+shapiro.test(age_no)
+
+# Histogrammes des distributions d'âge
+hist(age_yes)
+hist(age_no)
+
+# Transformations : log
+age_yes_log <- log(age_yes)
+age_no_log <- log(age_no)
+
+var.test(age_yes_log, age_no_log)
+shapiro.test(age_yes_log)
+shapiro.test(age_no_log)
+hist(age_yes_log)
+hist(age_no_log)
+
+# Transformations : carré
+age_yes_carre <- age_yes^2
+age_no_carre <- age_no^2
+
+var.test(age_yes_carre, age_no_carre)
+shapiro.test(age_yes_carre)
+shapiro.test(age_no_carre)
+
+# Test de Wilcoxon (non paramétrique)
+wilcox.test(age_yes, age_no, alternative = "greater")
+
+
+
+
+
 # ##### Clustering #####
 
 # 1) Récupérer nos variables catégorielles
 
 data_mca = data[2:12]
 
-# 2) ⚙️ Exécution de l'ACM
+# 2) Exécution de l'ACM
 res_mca <- MCA(data_mca, graph = FALSE)
 
 res_mca$eig
 #pourcentage de variance axe 1: 17.3
 #pourcentage de variance axe 2 : 9.6 # variance cumulée totale : 26.8 %
-#pourcentage de variance axe 3 : 6.3 # variance cumulée totale : 33.2 %
-
 
 # 3) 📐 Récupération des 5 premières dimensions
 mca_coords <- res_mca$ind$coord[, 1:5]
@@ -132,74 +195,5 @@ plot_ly(plot_data, x = ~Dim1, y = ~Dim2, z = ~Dim3,
         type = "scatter3d", mode = "markers") %>%
   layout(title = "Clusters sur les 3 premières dimensions ACM")
 
-# Comparaison des âges selon la récidive
 
-age_yes <- data$Age[data$Recurred == "Yes"]
-age_no  <- data$Age[data$Recurred == "No"]
-
-# Test de variance
-var.test(age_yes, age_no)
-
-# Test de normalité
-shapiro.test(age_yes)
-shapiro.test(age_no)
-
-# Histogrammes des distributions d'âge
-hist(age_yes)
-hist(age_no)
-
-# Transformations : log
-age_yes_log <- log(age_yes)
-age_no_log <- log(age_no)
-
-var.test(age_yes_log, age_no_log)
-shapiro.test(age_yes_log)
-shapiro.test(age_no_log)
-hist(age_yes_log)
-hist(age_no_log)
-
-# Transformations : carré
-age_yes_carre <- age_yes^2
-age_no_carre <- age_no^2
-
-var.test(age_yes_carre, age_no_carre)
-shapiro.test(age_yes_carre)
-shapiro.test(age_no_carre)
-
-# Test de Wilcoxon (non paramétrique)
-wilcox.test(age_yes, age_no, alternative = "greater")
-
-# Affichage de tous les barplots des variables qualitatives
-var_quali <- c("Gender", "Hx_Radiothreapy", "Adenopathy", "Pathology", "Focality", 
-               "Risk", "T", "N", "M", "Stage", "Response", "Recurred")
-
-plots <- list()
-for (var in var_quali) {
-  p <- ggplot(data, aes_string(x = var)) +
-    geom_bar(fill = "#0073C2FF") +
-    ggtitle(paste("Distribution des modalités pour", var)) +
-    xlab(var) + ylab("Nombre d'individus") +
-    theme_minimal() +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
-  plots[[var]] <- p
-}
-wrap_plots(plots, ncol = 3)  # Affichage organisé 
-#(utiliser le "zoom" pour voir le résutlat bien affiché)
-
-# Taux global de récidive
-nrow(data[data$Recurred == 'Yes', ]) / nrow(data)  # ~28 %
-
-# Pourcentage de récidive pour chaque modalité
-for (var in var_quali) {
-  cat("\nVariable:", var, "\n")
-  for (mod in unique(data[[var]])) {
-    total <- nrow(data[data[[var]] == mod, ])
-    rec <- nrow(data[data$Recurred == "Yes" & data[[var]] == mod, ])
-    perc <- round((rec / total) * 100, 1)
-    cat("  Modalité:", mod, 
-        "- Total:", total, 
-        "- Récidives:", rec, 
-        "- % Récidive:", perc, "%\n")
-  }
-}
 
--- data.preparation/script.r
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-#test
-jd <- read.delim("~/Documents/projet/data/jeu_de_donnees_ebird.txt")
-
-jdp <- jd %>%
-  select(COMMON.NAME, OBSERVATION.DATE, TIME.OBSERVATIONS.STARTED, OBSERVATION.COUNT) %>%
-  mutate(heure = lubridate::hour(hms::as_hms(TIME.OBSERVATIONS.STARTED)))
-
-write_csv(jdp, "~/Documents/projet/data.preparation/jeu_donnée_v1.csv")
